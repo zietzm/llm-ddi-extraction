16865ce72174e62bfbc9dd48a24c029d8832fd65
zietzm <michael.zietz@gmail.com>
2024-06-27 13:56:18 -0500

    refactor: clean up get and register methods

--- src/undina_llm/db.py
+++ src/undina_llm/db.py
@@ -1,42 +1,22 @@
-from typing import Literal
-
 from sqlmodel import Session, SQLModel, create_engine, select
 
 from undina_llm.models import DrugLabel, Prompt, Response, SystemPrompt
 
-SQLITE_FILE = "sqlite:///undina_llm.db"
-
 
 class SessionManager:
-    def __init__(self):
-        self.engine = create_engine(SQLITE_FILE)
+    def __init__(self, db_string: str):
+        self.engine = create_engine(db_string)
         SQLModel.metadata.create_all(self.engine)
 
     def get_prompt(self, prompt: str) -> Prompt:
         with Session(self.engine) as session:
             return session.exec(select(Prompt).where(Prompt.prompt == prompt)).one()
 
-    def register_prompt(self, prompt: str) -> Prompt:
-        prompt_obj = Prompt(prompt=prompt)
-        with Session(self.engine) as session:
-            session.add(prompt_obj)
-            session.commit()
-            session.refresh(prompt_obj)
-            return prompt_obj
-
     def get_system_prompt(self, system_prompt: str) -> SystemPrompt:
         with Session(self.engine) as session:
             return session.exec(
                 select(SystemPrompt).where(SystemPrompt.prompt == system_prompt)
             ).one()
-
-    def register_system_prompt(self, system_prompt: str) -> SystemPrompt:
-        system_prompt_obj = SystemPrompt(prompt=system_prompt)
-        with Session(self.engine) as session:
-            session.add(system_prompt_obj)
-            session.commit()
-            session.refresh(system_prompt_obj)
-            return system_prompt_obj
 
     def get_drug_label(self, drug_label: DrugLabel) -> DrugLabel:
         with Session(self.engine) as session:
@@ -47,6 +27,32 @@ class SessionManager:
                     & (DrugLabel.spl_version == drug_label.spl_version)
                 )
             ).one()
+
+    def get_response(self, response: Response) -> Response | None:
+        with Session(self.engine) as session:
+            return session.exec(
+                select(Response).where(
+                    (Response.prompt == response.prompt)
+                    & (Response.drug_label == response.drug_label)
+                    & (Response.system_prompt == response.system_prompt)
+                )
+            ).one_or_none()
+
+    def register_prompt(self, prompt: str) -> Prompt:
+        prompt_obj = Prompt(prompt=prompt)
+        with Session(self.engine) as session:
+            session.add(prompt_obj)
+            session.commit()
+            session.refresh(prompt_obj)
+            return prompt_obj
+
+    def register_system_prompt(self, system_prompt: str) -> SystemPrompt:
+        system_prompt_obj = SystemPrompt(prompt=system_prompt)
+        with Session(self.engine) as session:
+            session.add(system_prompt_obj)
+            session.commit()
+            session.refresh(system_prompt_obj)
+            return system_prompt_obj
 
     def register_drug_label(self, drug_label: DrugLabel) -> DrugLabel:
         with Session(self.engine) as session:
@@ -64,25 +70,7 @@ class SessionManager:
                 session.refresh(drug_label)
             return drug_labels
 
-    def register_response(
-        self,
-        system_prompt: SystemPrompt,
-        prompt: Prompt,
-        drug_label: DrugLabel,
-        section: Literal["DI", "CO"],
-        model: str,
-        temperature: float,
-        response: str,
-    ) -> None:
-        with Session(self.engine) as session:
-            response_obj = Response(
-                system_prompt=system_prompt,
-                prompt=prompt,
-                drug_label=drug_label,
-                section=section,
-                model=model,
-                temperature=temperature,
-                response=response,
-            )
-            session.add(response_obj)
+    def register_response(self, response: Response) -> None:
+        with Session(self.engine, expire_on_commit=False) as session:
+            session.add(response)
             session.commit()
